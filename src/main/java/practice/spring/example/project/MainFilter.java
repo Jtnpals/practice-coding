0e981320b7e1d3510eb5fa66099d95fe6adc506d
jtnpals <jtnpals@gmail.com>
2019-07-09 22:01:16 +0900

    Add url and query log

--- src/main/java/practice/spring/example/project/MainFilter.java
+++ src/main/java/practice/spring/example/project/MainFilter.java
@@ -3,6 +3,7 @@ package practice.spring.example.project;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Slf4j
@@ -15,6 +16,12 @@ public class MainFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         log.info("****** Before filter ******");
+        if(request instanceof HttpServletRequest){
+            String url = ((HttpServletRequest) request).getRequestURI();
+            String queryString  = ((HttpServletRequest) request).getQueryString();
+            log.info("url:: " + url);
+            log.info("query:: " + queryString);
+        }
         chain.doFilter(request,response);
         log.info("****** After filter ******");
 
