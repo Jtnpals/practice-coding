3e775760c8b3e5d2c58b15497925c0c2c3285f93
sumin jin <jtnpals@gmail.com>
2021-04-22 17:52:06 +0900

    Add simple polling pages

--- test/default/views.py
+++ test/default/views.py
@@ -1,31 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+from django.views import generic
 
-from default.models import Question
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('default/index.html')
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'default/index.html', context)
-
-
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'default/detail.html', {'question': question})
-
-
-def result(request, question_id):
-    response = HttpResponse("You're looking at question %s,")
-    return HttpResponse(response % question_id)
+from default.models import Question, Choice
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'default/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+    return HttpResponseRedirect(reverse('default:results', args=(question.id,)))
+
+
+class IndexView(generic.ListView):
+    template_name = 'default/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five publishd question."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'default/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'default/results.html'
