ee412742c9b1a3e67884e68a56cdfaa71686e182
sumin jin <jtnpals@gmail.com>
2021-04-26 19:14:39 +0900

    Add ListView templates

--- bookmark/bookmark/models.py
+++ bookmark/bookmark/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 class Bookmark(models.Model):
     site_name = models.CharField(max_length=100)
     url = models.URLField('Site URL')
+
     def __str__(self):
         return "name : " + self.site_name + ", url : " + self.url
