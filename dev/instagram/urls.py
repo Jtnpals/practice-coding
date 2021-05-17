78d49731e58cd10d445bccee1b4513fef9c4459b
sumin jin <jtnpals@gmail.com>
2021-05-17 15:21:52 +0900

    Add custom converter

--- dev/instagram/urls.py
+++ dev/instagram/urls.py
@@ -1,9 +1,24 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, register_converter
 
 from instagram import views
+
+
+class YearConverter:
+    regex = r'20\d{2}'
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return str(value)
+
+
+register_converter(YearConverter, 'year')
 
 urlpatterns = [
     path('', views.post_list),
     path('<int:pk>/', views.post_detail),
+    path('archives/<year:year>/', views.archives_year),
+    # re_path(r'archives/(?P<year>20\d{2})/', views.archives_year),
     # re_path(r'(?P<pk>\d+)/$', views.post_detail),
 ]
--- dev/instagram/views.py
+++ dev/instagram/views.py
@@ -1,16 +1,24 @@
+from django.http import HttpResponse
+from django.views.generic import ListView
 from django.shortcuts import render
 
 # Create your views here.
 from instagram.models import Post
 
+post_list = ListView.as_view(model=Post)
 
-def post_list(request):
-    qs = Post.objects.all()
-    q = request.GET.get('q', '')
-    if q:
-        qs = qs.filter(message__icontains=q)
-    return render(request, 'instagram/post_list.html', {'post_list': qs, 'q': q})
+
+# def post_list(request):
+#     qs = Post.objects.all()
+#     q = request.GET.get('q', '')
+#     if q:
+#         qs = qs.filter(message__icontains=q)
+#     return render(request, 'instagram/post_list.html', {'post_list': qs, 'q': q})
 
 
 def post_detail(request, pk):
     pass
+
+
+def archives_year(request, year):
+    return HttpResponse(f'{year} archives')
