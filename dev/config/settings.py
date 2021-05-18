f03d1c0571cab6dca55d18147247787e902db4c3
sumin jin <jtnpals@gmail.com>
2021-05-18 16:45:22 +0900

    Use collectstatic command

--- dev/config/settings.py
+++ dev/config/settings.py
@@ -140,7 +140,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'config', 'static'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
