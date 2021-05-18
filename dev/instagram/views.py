a74f6f17365a1ff4be0ec213844f5c0786b95d2a
sumin jin <jtnpals@gmail.com>
2021-05-18 11:51:18 +0900

    Use decorator

--- dev/instagram/views.py
+++ dev/instagram/views.py
@@ -1,11 +1,22 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpRequest, Http404
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from instagram.models import Post
 
-post_list = ListView.as_view(model=Post, paginate_by=10)
+
+# post_list = login_required(ListView.as_view(model=Post, paginate_by=10))
+# @method_decorator(login_required, name='dispatch')
+class PostListView(LoginRequiredMixin, ListView):
+    model = Post
+    paginate_by = 10
+
+
+post_list = PostListView.as_view()
 
 
 # def post_list(request):
