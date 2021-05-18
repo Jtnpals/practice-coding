34868ac7e74339fa1a39f4c1b645d1cd3e0bfb7b
sumin jin <jtnpals@gmail.com>
2021-05-18 10:55:24 +0900

    Add CBV API(DetailView)

--- dev/instagram/views.py
+++ dev/instagram/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest, Http404
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from instagram.models import Post
@@ -16,8 +16,30 @@ post_list = ListView.as_view(model=Post)
 #     return render(request, 'instagram/post_list.html', {'post_list': qs, 'q': q})
 
 
-def post_detail(request, pk):
-    pass
+# def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+#     post = get_object_or_404(Post, pk=pk)
+#     # try:
+#     #     post = Post.objects.get(pk=pk)
+#     # except Post.DoesNotExist:
+#     #     raise Http404
+#     return render(request, 'instagram/post_detail.html', {'post': post, })
+
+# post_detail = DetailView.as_view(
+#     model=Post,
+#     queryset=Post.objects.filter(is_public=True))
+
+class PostDetailView(DetailView):
+    model = Post
+    # queryset = Post.objects.filter(is_public=True)
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if not self.request.user.is_authenticated:
+            qs = qs.filter(is_public=True)
+        return qs
+
+
+post_detail = PostDetailView.as_view()
 
 
 def archives_year(request, year):
