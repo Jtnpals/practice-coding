d7bade6d5e010d19e0e1a6e20f4872682f21a29d
sumin jin <jtnpals@gmail.com>
2021-06-24 14:27:28 +0900

    Add login_form.html

--- noSPA/accounts/views.py
+++ noSPA/accounts/views.py
@@ -1,7 +1,16 @@
+from django.contrib.auth.views import LoginView, logout_then_login
+from django.contrib.auth import login as auth_login
 from django.shortcuts import render, redirect
 from django.contrib import messages
 # Create your views here.
 from accounts.forms import SignupForm
+
+login = LoginView.as_view(template_name='accounts/login_form.html')
+
+
+def logout(reqeust):
+    messages.success(reqeust, "로그아웃되었습니다.")
+    return logout_then_login(reqeust)
 
 
 def signup(request):
@@ -9,6 +18,7 @@ def signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             signed_user = form.save()
+            auth_login(request, signed_user)
             # signed_user.send_welcome_email()  # FIXME: celery로 비동기 처리해야함
             messages.success(request, '회원가입 환영합니다.')
             next_url = request.GET.get('next', '/')
