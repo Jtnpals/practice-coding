66bb94e259651036f1c55bf1eb79d1e40db62508
sumin jin <jtnpals@gmail.com>
2021-07-06 16:04:43 +0900

    Add post serializer

---
+++ drf/instagram/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('post', views.PostViewSet)
+
+urlpatterns = [
+    path('public/', views.public_post_list),
+    path('', include(router.urls)),
+]
