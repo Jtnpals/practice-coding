a4d85a26591af282a980b23dc9f18320fe7a9e0a
sumin jin <jtnpals@gmail.com>
2021-07-06 10:13:21 +0900

    Add index.html

--- noSPA/config/urls.py
+++ noSPA/config/urls.py
@@ -3,13 +3,13 @@ from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.urls import path, include
 from django.conf import settings
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', login_required(TemplateView.as_view(template_name='root.html')), name='root'),
+    path('', login_required(RedirectView.as_view(pattern_name='instagram:index')), name='root'),
     path('accounts/', include('accounts.urls')),
-    path('', include('instagram.urls')),
+    path('instagram/', include('instagram.urls')),
 ]
 
 if settings.DEBUG:
