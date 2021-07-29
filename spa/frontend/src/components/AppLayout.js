1be6dedf1a0af0395437f1d50cbbf37a8c78c120
sumin jin <jtnpals@gmail.com>
2021-07-29 10:01:33 +0900

    Add login function

--- spa/frontend/src/components/AppLayout.js
+++ spa/frontend/src/components/AppLayout.js
@@ -1,14 +1,40 @@
 import React from "react";
-import AppFooter from "./AppFooter";
-import AppHeader from "./AppHeader";
+import { Input, Menu } from "antd";
+import "./AppLayout.scss"
+import StoryList from "./StoryList";
+import SuggestionList from "./SuggestionList";
 
 function AppLayout({children}){
     return(
-        <>
-            <AppHeader/>
-                {children}
-            <AppFooter/>
-        </>
+        <div className="app">
+            <div className="header">
+                <h1>Instagram</h1>
+                <div className="search">
+                    <Input.Search placeholder="검색어"/>
+                </div>
+                <div className="topnav">
+                    <Menu mode="horizontal">
+                        <Menu.Item key="menu1">
+                            메뉴1
+                        </Menu.Item>
+                        <Menu.Item key="menu2">
+                            메뉴2
+                        </Menu.Item>
+                        <Menu.Item key="menu3">
+                            메뉴3
+                        </Menu.Item>
+                    </Menu>
+                </div>
+            </div>
+            <div className="sidebar">
+                <StoryList style={{ marginBottom: '1rem'}}/>
+                <SuggestionList style={{ marginBottom: '1rem'}}/>
+            </div>
+            <div className="contents">{children}</div>
+            <div className="footer">
+                &copy; 2021. test
+            </div>
+        </div>
     )
 }
 
