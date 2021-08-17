41f6811ffa1e9312d7304ad93805669a590ff349
sumin jin <jtnpals@gmail.com>
2021-08-17 13:14:23 +0900

    Add MyForm

--- typescript/pages/_app.tsx
+++ typescript/pages/_app.tsx
@@ -1,12 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Counter from "./Counter";
+import MyForm from "./MyForm";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
   return (
     <>
       <Component {...pageProps} />
       <Counter />
+      <MyForm onSubmit={onSubmit} />
     </>
   );
 }
