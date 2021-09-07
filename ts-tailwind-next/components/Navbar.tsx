e363963bd605d84b7ca7612974c94299a2c92f87
sumin jin <jtnpals@gmail.com>
2021-09-07 09:46:57 +0900

    Update Navbar

--- ts-tailwind-next/components/Navbar.tsx
+++ ts-tailwind-next/components/Navbar.tsx
@@ -2,20 +2,36 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="logo">
-        <h1>Test List</h1>
-      </div>
-
-      <Link href="/">
-        <a>home</a>
-      </Link>
-      <Link href="/about">
-        <a>about</a>
-      </Link>
-      <Link href="/list">
-        <a>list</a>
-      </Link>
+    <nav className="container m-auto bg-gray-400 flex text-xl items-center h-14">
+      <div className="logo text-4xl ml-4 mr-5">amuamu</div>
+      <li className="flex gap-2">
+        <ul>
+          <Link href="/">
+            <a>home</a>
+          </Link>
+        </ul>
+        <ul>
+          <Link href="/about">
+            <a>about</a>
+          </Link>
+        </ul>
+        <ul>
+          <Link href="/list">
+            <a>list</a>
+          </Link>
+        </ul>
+      </li>
+      <li className="flex ml-auto gap-3 mr-4">
+        <ul>
+          <a>user</a>
+        </ul>
+        <ul>
+          <a>wish</a>
+        </ul>
+        <ul>
+          <a>some</a>
+        </ul>
+      </li>
     </nav>
   )
 }
--- ts-tailwind-next/pages/index.tsx
+++ ts-tailwind-next/pages/index.tsx
@@ -1,16 +1,10 @@
-import Head from 'next/head'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-
 export default function Home() {
   return (
     <div>
-      <Navbar />
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iure ea
       cupiditate autem inventore tenetur vitae. Nesciunt veniam laudantium
       doloremque esse autem nihil aliquam sunt dicta a possimus, perferendis
       atque.
-      <Footer />
     </div>
   )
 }
