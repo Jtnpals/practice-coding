040b9a5ad8f97da332d5dd2a8cbe1bbf2f01d6f9
Jtnpals <Jtnpals@gmail.com>
2021-10-28 11:45:51 +0900

    Sample class

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-interface Human {
-  name: string,
-  age: number,
-  gender: string
+class Human {
+  public name: string
+  public age: number
+  public gender: string
+  constructor(name:string, age:number, gender:string){
+    this.name = name
+    this.age = age
+    this.gender = gender
+  }
 }
+
+const lynn = new Human("Lynn", 17, "female")
 
 const person = {
   name: 'tom',
@@ -19,5 +26,6 @@ const sayHi = (person:Human): string => {
 }
 
 console.log(sayHi(person))
+console.log(sayHi(lynn))
 
 export {}
